46369a5f12ee44e9cddfafa7b4397b20e891de4e
chowyiyin <yiyin.chow@gmail.com>
2020-07-15 16:25:32 +0800

    Change to editable proportion of dataset splits and read data from multiple subdirectories

--- language_model.py
+++ language_model.py
@@ -1,4 +1,4 @@
-from io import open
+import io
 import torch
 import torch.nn as nn
 import string
@@ -10,32 +10,38 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+import shutil
 
 # read data
 def readFromFile(filename):
-    text = open(filename, encoding='utf-8').read().strip()
-    return text
+    try:
+      text = open(filename, encoding='utf-8').read().strip()
+    except:
+      print(filename)
+      return '',''
+    letters = ''.join(set(text))
+    lines = [text[i:i + seq_len] for i in range(0, len(text), seq_len)]
+    return letters, lines
 
 # preprocessing
 def readLinesFromData(directory, seq_len):
     letters = ''
     lines = []
-    counter = 0
-    for filename in os.listdir(directory):
-        counter += 1
-        text = readFromFile(os.path.join(directory, filename))
-        letters = ''.join(set(letters + text))
-        lines.extend([text[i:i + seq_len] for i in range(0, len(text), seq_len)])
-        if counter >= 20: # to test network with smaller sample
+    for root, directories, filenames in os.walk(directory):
+        for directory in directories:
+            directory_letters, directory_lines = readLinesFromData(directory, seq_len)
+            letters = ''.join(set(letters + directory_letters))
+            lines.extend(directory_lines)
             break
+        counter = 0
+        for filename in filenames:
+            counter += 1
+            file_letters, file_lines = readFromFile(os.path.join(root, filename))
+            letters = ''.join(set(letters + file_letters))
+            lines.extend(file_lines)
+            if counter >= 10:
+                break
     return letters, lines
-
-seq_len = 75
-directory = os.path.join(os.getcwd(), 'maildir/allen-p/_sent_mail')
-letters, lines = readLinesFromData(directory, seq_len)
-#letters = string.ascii_letters
-#lines = ['hey how are you', 'good im fine']
-num_of_letters = len(letters) + 2
 
 # prepare input and target tensors
 def convertToOneHotEncoding(index_in_letters):
@@ -64,6 +70,7 @@ def createTargetTensor(line, max_length):
 
 def getBatches(batch_size, data, seq_len):
     num_of_full_batches = int(float(len(data)) / float(batch_size))
+    print(num_of_full_batches)
     data = data[:num_of_full_batches * batch_size]
     batches = []
     counter = 0
@@ -76,38 +83,25 @@ def getBatches(batch_size, data, seq_len):
         batch = (input_seq, target_seq)
         batches.append(batch)
         counter += batch_size
-    return batches
+        yield input_seq, target_seq
 
-
-def splitData(data, batch_size):
-    training_size = int(len(data) * 4/5 * 4/5)
-    validation_size = int(len(data) * 4/5 * 1/5)
-    test_size = int(len(data) * 1/5)
-
-    training_data = data[:training_size]
-    validation_data = data[training_size: training_size + validation_size]
-    test_data = data[training_size + validation_size: training_size + validation_size + test_size]
+def splitData(data, batch_size, segmentation=1000, training_proportion=0.7,
+              validation_proportion=0.2, test_proportion=0.1):
+    training_data = []
+    validation_data = []
+    test_data = []
+    training_size = int(training_proportion * segmentation)
+    validation_size = int(validation_proportion * segmentation)
+    test_size = int(test_proportion * segmentation)
+    for i in range(0, len(data), segmentation):
+      training_end = i + training_size
+      training_data.extend(data[i: i + training_end])
+      validation_end = training_end + validation_size
+      validation_data.extend(data[training_end: validation_end])
+      test_end = validation_end + test_size
+      test_data.extend(data[validation_end: test_end])
     
-    training_batches = getBatches(batch_size, training_data, seq_len)
-    validation_batches = getBatches(batch_size, validation_data, seq_len)
-    test_batches = getBatches(batch_size, test_data, seq_len)
-    
-    return training_batches, validation_batches, test_batches
-
-batch_size = 32
-
-training_batches, validation_batches, test_batches = splitData(lines, batch_size)
-#training_batches = getBatches(batch_size, lines, seq_len)
-print("Finished preprocessing....")
-
-is_cuda = torch.cuda.is_available()
-
-if is_cuda:
-    device = torch.device("cuda")
-    print("GPU is available")
-else:
-    device = torch.device("cpu")
-    print("GPU not available, CPU used")
+    return training_data, validation_data, test_data
 
 # define Model
 class Model(nn.Module):
@@ -146,13 +140,16 @@ class Model(nn.Module):
         cell_state = torch.zeros(self.num_of_layers, batch_size, self.hidden_dim)
         return cell_state
 
-# hyperparameters
-num_of_epochs = 10
-learning_rate = 0.001
-hidden_dim = 100
-model = Model(input_size=num_of_letters, output_size=num_of_letters, hidden_dim=hidden_dim, num_of_layers=2)
-model.to(device)
-softmax = nn.functional.softmax
+def save_checkpoint(state, is_best, checkpoint):
+   filepath = os.path.join(checkpoint, 'last.pth.tar')
+   if not os.path.exists(checkpoint):
+       print("Checkpoint Directory does not exist! Making directory {}".format(checkpoint))
+       os.mkdir(checkpoint)
+   else:
+       print("Checkpoint Directory exists! ")
+   torch.save(state, filepath)
+   if is_best:
+       shutil.copyfile(filepath, os.path.join(checkpoint, 'best.pth.tar'))
 
 def plot_graph(all_losses, validation_losses):
     plt.figure()
@@ -160,12 +157,12 @@ def plot_graph(all_losses, validation_losses):
     plt.plot(validation_losses)
     plt.show()
 
-def getValidationAccuracyAndLoss(model, criterion, validation_batches):
+def getValidationAccuracyAndLoss(model, criterion, validation_data, batch_size, seq_len):
     model.eval()
     with torch.no_grad():
         validation_loss = []
         matches, total = 0, 0
-        for input_seq, target_seq in validation_batches:
+        for input_seq, target_seq in getBatches(batch_size, validation_data, seq_len):
             input_seq.to(device)
             output, hidden = model(input_seq)
             criterion_input = output.transpose(1, 2)
@@ -180,69 +177,104 @@ def getValidationAccuracyAndLoss(model, criterion, validation_batches):
     model.train()
     return validation_accuracy, np.mean(validation_loss)
 
-# loss function
-criterion = nn.CrossEntropyLoss()
-optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
-def train(model, training_batches, validation_batches):
+def printResults(epoch, num_of_epochs, total_loss, validation_loss, 
+                 validation_accuracy, perplexity):
+  print('Epoch: {}/{}.............'.format(epoch, num_of_epochs), end=' ')
+  print("Loss: {:.4f}".format(total_loss), end=' ')
+  print("Val Loss: {:.4f}".format(validation_loss), end=' ')
+  print("Accuracy: {:.2f}".format(validation_accuracy ) + "%", end=' ')
+  print("Perplexity: {:.4f}".format(perplexity))
+
+def createCheckpoint(model, optimiser, letters):
+  return {'model': model, 'state_dict': model.state_dict(),
+          'optimizer' : optimiser.state_dict(), 'letters': letters}
+
+
+def train(model, criterion, optimiser, training_data, validation_data, batch_size, seq_len, letters):
     all_losses = []
     validation_losses = []
+    highest_accuracy = 0
     for epoch in range(1, num_of_epochs + 1):
-        for input_seq, target_seq in training_batches:
-            total_loss = 0
-            optimiser.zero_grad()
-            input_seq.to(device)
-            output, hidden = model(input_seq)
-            criterion_input = output.transpose(1, 2)
-            loss = criterion(criterion_input, target_seq.long())
-            total_loss += loss
-            loss.backward()
-            optimiser.step()
-        if epoch%10 == 0:
-            validation_accuracy, validation_loss = getValidationAccuracyAndLoss(model, criterion, validation_batches)
-            validation_losses.append(validation_loss)
-            all_losses.append(total_loss)
-            print('Epoch: {}/{}.............'.format(epoch, num_of_epochs), end=' ')
-            print("Loss: {:.4f}".format(total_loss), end=' ')
-            print("Val Loss: {:.4f}".format(validation_loss), end=' ')
-            print("Accuracy: {:.2f}".format(validation_accuracy * 100) + "%", end=' ')
-            print("Perplexity: {:.4f}".format(torch.exp(total_loss)))
+      start = time.time()
+      for input_seq, target_seq in getBatches(batch_size, training_data, seq_len):
+          total_loss = 0
+          optimiser.zero_grad()
+          input_seq.to(device)
+          output, hidden = model(input_seq)
+          criterion_input = output.transpose(1, 2)
+          loss = criterion(criterion_input, target_seq.long())
+          total_loss += loss
+          loss.backward()
+          optimiser.step()
+      if epoch%10 == 0:
+          validation_accuracy, validation_loss = getValidationAccuracyAndLoss(
+              model, criterion, validation_data,
+              batch_size, seq_len)
+          # save losses
+          validation_losses.append(validation_loss)
+          all_losses.append(total_loss)
+          checkpoint = createCheckpoint(model, optimiser, letters)
+          is_best = validation_accuracy > highest_accuracy
+          save_checkpoint(checkpoint, is_best, 'checkpoints')
+          if is_best:
+            highest_accuracy = validation_accuracy
+          
+          printResults(epoch, num_of_epochs, total_loss, validation_loss,
+                        validation_accuracy * 100, torch.exp(total_loss))
+      end = time.time()
+      time_for_one_epoch = end - start
+      print("epoch: " + str(epoch) + " " + "time: " + str(time_for_one_epoch) + "s")
     plot_graph(all_losses, validation_losses)
 
-train(model, training_batches, validation_batches)
-# save model
-checkpoint = {'model': model, 'state_dict': model.state_dict(), 'optimiser': optimiser.state_dict}
-torch.save(model.state_dict(), os.path.join(os.getcwd(), 'finished_training_checkpoint.pt'))
-
-# predict
-
-# Takes in the model and character as arguments 
-# and returns the next character prediction and hidden state
-def predict(model, characters):
-    print(characters) # previous characters that next character is dependent on
-    characters = createInputTensor(characters, len(characters))
-    characters.to(device)
+def getDevice():
+    is_cuda = torch.cuda.is_available()
+    device = None
     
-    output, hidden = model(characters)
-    last_output = output[-1][-1]
-    prob = nn.functional.softmax(last_output, dim=0).data # can stop here to get probabilities
-    print(prob)
+    if is_cuda:
+        device = torch.device("cuda")
+        print("GPU is available")
+    else:
+        device = torch.device("cpu")
+        print("GPU not available, CPU used")
 
-    # Take the character with the highest probability score from the output
-    char_ind = torch.max(prob, dim=0)[1].item()
-    if char_ind == num_of_letters - 1 or char_ind == 0:
-        return '', hidden
-    return letters[char_ind - 1], hidden
+    return device
 
-# Takes the desired output length and input characters as arguments, 
-# returning the produced string
-def sample(model, output_length, start_chars='Message'):
-    model.eval() # eval mode
-    chars = [ch for ch in start_chars]
-    remaining_length = output_length - len(chars)
-    for ii in range(remaining_length):
-        char, h = predict(model, chars)
-        chars.append(char)
+def main():
+    # define lines and letters
+    seq_len = 75
+    directory = os.path.join(os.getcwd(), 'maildir/allen-p/_sent_mail')
+    letters, lines = readLinesFromData(directory, seq_len)
+    num_of_letters = len(letters) + 2
+    print(len(lines))
+    print(num_of_letters)
 
-    return ''.join(chars)
+    # prepare data
+    batch_size = 32
+    training_data, validation_data, test_data = splitData(lines, batch_size, segmentation=10)
 
-print(sample(model, 150))
+    print("Finished preprocessing....")
+
+    device = getDevice()
+
+    # hyperparameters
+    num_of_epochs = 10
+    learning_rate = 0.001
+    hidden_dim = 50
+
+    # define model
+    model = Model(input_size=num_of_letters, output_size=num_of_letters, hidden_dim=hidden_dim, num_of_layers=2)
+    model.to(device)
+    softmax = nn.functional.softmax
+
+    # loss function
+    criterion = nn.CrossEntropyLoss()
+    optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    train(model, criterion, optimiser, training_data, validation_data, batch_size, seq_len)
+    
+    # save model
+    final_checkpoint = createCheckpoint(model, optimiser, letters)
+    torch.save(final_checkpoint, os.path.join(os.getcwd(), 'finished_training_checkpoint.pt'))
+
+if __name__ == "__main__":
+    main()
